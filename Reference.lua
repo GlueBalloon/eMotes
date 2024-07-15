88f8424d48e3641d9f78dc6a6409f0990b8ae677
JWC on iPhone <jwc@jessewonderclark.com>
2024-07-15 17:44:56 -0400

    add a bunch of assets

---
+++ Reference.lua
@@ -0,0 +1,118 @@
+--[[
+1. Happy & Joyful Set
+
+	•	Emojis: 😀, 😃, 😄, 🙃, 🙂, 😏, 😁, 😆, 😅, 😊, 😇, 😉, 😌, 🤭, 😋, 😛, 😝, 😜, 🤪, 🤓, 😎, 
+	•	Sound: Light, bubbly sound or a short giggle.
+😙, 😚,
+😠, 
+🤫,
+🥴
+Happy
+Curious & Thoughtful
+Neutral & Unimpressed
+Surprised
+Grouchy
+four special cases
+
+2. Curious & Thoughtful Set
+
+	•	Emojis: 🤨, 🧐, 🤔, 😗,
+	•	Sound: A pondering hum or a sound indicating curiosity.
+
+3. Neutral & Unimpressed Set
+
+	•	Emojis: 😒, 😐, 😑, 😶, 🙄, 😬, 😕,
+	•	Sound: A short, indifferent sound, like a mild ‘hmm’.
+
+4. Sleepy Set
+
+	•	Emojis: 😴, 🤤
+	•	Sound: A yawn or a gentle cough/sneeze sound.
+Unwell
+😷, 🤒, 🤕
+5. Sad & Displeased Set
+
+	•	Emojis: 😔, 😟, 🙁, 🥺, 😢, 🤥
+	•	Sound: A low, sad tone or a short sigh.
+
+6. Surprised & Shocked Set
+
+	•	Emojis: 😳, 😯, 😲, 🤐
+	•	Sound: A quick gasp or a surprised exclamation.
+
+Happy
+
+	•	Emojis: 😀, 😃, 😄, 😁, 😆, 😅, 😊, 😇, 😉, 😌, 😗, 😙, 😚, 😋, 😛, 😝, 😜, 🤪, 😍, ❤️, 🧡, 💛, 💚, 💙, 💜, 🥳, 😻
+	•	Sound: A cheerful giggle or upbeat chime.
+
+Curious & Thoughtful
+
+	•	Emojis: 🤨, 🧐, 🤓, 😎, 🤔, 🤭, 🤫, 🤥, 💭, 🕵️, 🧠
+	•	Sound: A contemplative hum or a curious ‘hmm’.
+
+Neutral & Unimpressed
+
+	•	Emojis: 🙃, 🙂, 😏, 😒, 😐, 😑, 😶, 🙄, 😮‍💨
+	•	Sound: A short, indifferent ‘meh’ or soft ‘huh’.
+
+Surprised
+
+	•	Emojis: 😳, 😯, 😲, 🤐, 😮, 😧, 😦, 🤭, 😱
+	•	Sound: A quick gasp or a surprised ‘whoa’.
+
+Grouchy or Ouchy
+
+	•	Emojis: 😠, 😡, 🤬, 😤, 😣, 😖, 😫, 😩, 🥵, 🥶, 😰, 😨, 😥, 🤒, 🤕, 😓, 🤢, 🤧, 😷, 😑, 😐
+	•	Sound: A grumble or an ‘ouch’ sound.
+
+Special Cases
+
+	1.	Cowboy Emoji (🤠): A cheerful “yee-haw”.
+	•	Unique Sound: A clear and enthusiastic “yee-haw”.
+	2.	Ghost Emoji (👻): A playful boo or spooky laugh.
+	•	Unique Sound: A soft, ghostly “boo” or a light-hearted spooky laugh.
+	3.	Robot Emoji (🤖): Robotic beeping or a digital “hello”.
+	•	Unique Sound: A series of cute, robotic beeps or a synthesized voice saying “hello”.
+	4.	Alien Emoji (👽): A sci-fi space sound or alien greeting.
+	•	Unique Sound: A sci-fi blip or a quirky, otherworldly sound effect suggesting an alien greeting.
+
+Based on your requirements, let’s expand the categories to include “Feeling Sick” and “Party Time,” along with any other deducible categories that might fit several emojis. Emojis can indeed appear in more than one category, allowing for versatile sound associations.
+
+Feeling Sick
+
+	•	Emojis: 🤒, 🤕, 🤢, 🤮, 😷, 🤧
+	•	Sound: A weary sigh, a cough, or a sneezing sound.
+
+Party Time
+
+	•	Emojis: 🥳, 🎉, 🎊, 🍾, 🥂, 🎈, 🕺, 💃
+	•	Sound: Festive music, party horn, or people cheering.
+
+Additional Categories
+
+Love & Affection
+
+	•	Emojis: ❤️, 💛, 💚, 💙, 💜, 🧡, 😍, 😘, 💑, 💏, 💌
+	•	Sound: A heart beating sound or a kiss sound.
+
+Tired & Sleepy
+
+	•	Emojis: 😴, 🥱, 😪, 😌, 🛌
+	•	Sound: A yawn or snoring sound.
+
+Excited & Joyful
+
+	•	Emojis: 😆, 😁, 🤣, 😂, 😃, 🤩, 😜, 😝, 🤪, 😛
+	•	Sound: Laughter or an exclamatory “woo-hoo!”
+
+Anxious & Fearful
+
+	•	Emojis: 😟, 😰, 😨, 😱, 🥶, 😖, 😣
+	•	Sound: A suspenseful sound or a worried gulp.
+
+Thinking & Pondering
+
+	•	Emojis: 🤔, 🧐, 💭, 🤨
+	•	Sound: A pondering hum or a light bulb moment sound.
+
+]]
